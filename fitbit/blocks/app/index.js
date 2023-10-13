5732cbe0668ef1d80522ea15a243b05bb12b42d4
Ian Atkin <ian@versal.com>
2023-10-13 10:40:55 +0100

    finished up basic display

--- fitbit/blocks/app/index.js
+++ fitbit/blocks/app/index.js
@@ -1,14 +1,20 @@
 import clock from "clock";
 import document from "document";
 import { preferences } from "user-settings";
-//import * as util from "../resources/utils";
+import { today as toDay } from "user-activity";
+import * as util from "../resources/utils";
+
+const h = [];
+const m = [];
+const s = [];
+const sk = [];
+
+util.setup(h, m, s, sk, document);
+util.reset(h, m, s);
+util.resetSteps(sk);
 
 // Tick every second
 clock.granularity = "seconds";
-
-function ... {
-
-}
 
 function updateClock() {
   let pcd = (preferences.clockDisplay === "12h") ? 12 : 24;
@@ -17,7 +23,30 @@ function updateClock() {
   let minutes = today.getMinutes();
   let seconds = today.getSeconds();
 
-  ...
+  if (seconds == 0) {
+    util.reset(h, m, s);
+  }
+
+  if (hours == 0 && minutes == 0 && seconds == 0) {
+    util.resetSteps(sk);
+  }
+
+  for (let i = 1; i <= hours; i++) {
+    h[i].style.visibility = "visible";
+  }
+
+  for (let i = 1; i <= minutes; i++) {
+    m[i].style.visibility = "visible";
+  }
+
+  for (let i = 1; i <= seconds; i++) {
+    s[i].style.visibility = "visible";
+  }
+
+  let steps = (toDay.adjusted.steps < 30001) ? parseInt(toDay.adjusted.steps / 1000) : 30;
+  for (let i = 1; i <= steps; i++) {
+    sk[i].style.visibility = "visible";
+  }
 }
 
 // Update the clock every tick event
